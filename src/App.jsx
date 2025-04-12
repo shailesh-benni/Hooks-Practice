293900217d7fb574a9bf4ca8765416414a9288df
testing-gems <shailesh.butter@gmail.com>
2025-04-12 22:25:40 +0530

    useRef Hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import UseStateHook from './hooksExample/UseStateHook'
 import UseEffectHook from './hooksExample/UseEffectHook'
 import UseContextHook from './hooksExample/UseContextHook'
-import LoginContextProvider from './context/LoginContextProvider'//Achhese code organize karne k liye aisa karna padta hai context file wagera banana usme hook ko use karna...
+import LoginContextProvider from './context/LoginContextProvider'//Achhese code organize karne k liye aise karna padta hai context file wagera banake usme hook ko use karna...
+import UseRefHook from './hooksExample/UseRefHook'
 
 //Konsa Hook Chahiye usko niche app m use karke try karo 
 
 
 const App = () => {
+  {/*iss value bina UseContextHook m pass karke ChildContext m use kar sakte hai..*/}
   return (
-    <LoginContextProvider> {/*iss value bina UseContextHook m pass karke ChildContext m use kar sakte hai..*/}
-      <UseContextHook/>
-    </LoginContextProvider>
+    // <LoginContextProvider> 
+    //   <UseContextHook/>
+    // </LoginContextProvider>
+    <UseRefHook/>
   )
 }
 
---
+++ src/hooksExample/UseRefHook.jsx
@@ -0,0 +1,39 @@
+//the hook allow us to access DOM elements
+//for creating mutable elements which will not re-render the  components..
+import React, { useEffect, useRef } from 'react'
+import { useState } from 'react'
+
+const UseRefHook = () => {
+    const [name, setName] = useState("");
+    // const [counter, setCounter] = useState(0);   //Using Use State
+    const count = useRef(0);
+    console.log(count);
+    useEffect(()=>{
+        // setCounter((prev)=>prev+1); // Using use state
+        count.current = count.current+1;
+    });
+
+    const inputEle = useRef();
+     const handleClick=()=>{
+        console.log(inputEle);
+        inputEle.current.style.width = "300px";
+        inputEle.current.focus();
+     }
+
+  return (
+    <div>
+        <input type="text" onChange={(e)=>setName(e.target.value)} />
+        
+        <h2>Name: {name}</h2>
+        {/* <h2> this Count when useState is using : {counter}</h2> */} {/* This leads Infinite */}
+
+        <h2>Renders: {count.current}</h2>
+
+
+        <input type="text" ref={inputEle} />
+        <button onClick={handleClick}>Increase Width</button>
+    </div>
+  )
+}
+
+export default UseRefHook
